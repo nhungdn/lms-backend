78735de885c52345d607227846927da1c5a40346
nhungdn <nhungdoanbg191@gmail.com>
2026-05-24 23:03:01 +0700

    feat(category): add authorization check for category creation

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,16 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  ForbiddenException,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CategoryDto } from './category.dto';
 import { CategoryService } from './category.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { Role } from 'src/generated/prisma/client';
 
 @Controller({
   version: '1',
   path: 'categories',
 })
+@UseGuards(JwtAuthGuard)
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
-  create(@Body() categoryDto: CategoryDto) {
+  create(@Body() categoryDto: CategoryDto, @CurrentUser() user) {
+    if (user.role !== Role.ADMIN) {
+      throw new ForbiddenException(
+        'You do not have permission to create a category',
+      );
+    }
+
     return this.categoryService.createCategory(categoryDto);
   }
 }
